3a4d9755bbc138baeeb4a30c06e28838a6ac7e02
praveenjrm <praveen26cs@gmail.com>
2024-11-11 12:50:12 +0530

    code update

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "tfdeploy1" {
+provider "aws" {
+  region	=  "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
   ami	= "ami-0866a3c8686eaeeba"
   instance_type = "t3.micro"
 
   tags = {
-     Name = "codedeployvm1"
+     Name = "2nd-ec2"
   }
 }
