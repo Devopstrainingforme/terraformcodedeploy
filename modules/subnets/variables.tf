f7eeff127f138abdacdd70fcc84cdfd99b588fe3
praveenjrm <praveen26cs@gmail.com>
2024-11-13 22:17:46 +0530

    subnet update

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -1,11 +1,11 @@
 variable "cidr_sb1" {
-  default = "This is the subnet-1 cidr range"
+  description = "This is the subnet-1 cidr range"
   type = string
-  default = "10.0.0.0/24"
+  default = "10.0.0.0/26"
 }
 
 variable "zone_sb1" {
-  default = "This is the zone of subnet-1"
+  description = "This is the zone of subnet-1"
   type = string
   default = "us-east-1"
 }
