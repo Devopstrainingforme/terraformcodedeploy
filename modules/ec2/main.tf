d513d5aa7e6f794aea41d96396ab3ba83a6296f0
praveenjrm <praveen26cs@gmail.com>
2024-11-14 14:04:28 +0530

    code update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,8 +1,16 @@
+module "subnets" {
+  source = "./modules/subnets"
+}
+
+module "security-group" {
+  source = "./modules/security-group"
+}
+
 resource "aws_instance" "terraserver1" {
     ami = var.imageid
     name = var.instance_name
-    subnet_id = var.terrasubnet1
-    vpc_security_group_ids = var.terrasg1
+    subnet_id = module.subnet.subnet_id
+    vpc_security_group_ids = module.security-group.security-group_id
     instance_type = var.terrainstype
 
 root_block_device {
