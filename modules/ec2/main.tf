f2540c145549b1fe6518888ee041f9032ba3ed35
praveenjrm <praveen26cs@gmail.com>
2024-11-14 20:07:43 +0530

    code fix

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,10 +4,14 @@ resource "aws_instance" "terraserver1" {
     #vpc_security_group_ids = module.security-group.terrasg1_name
     instance_type = var.terrainstype
 
+/*
+
 root_block_device {
     volume_size = 16
     volume_type = "gp2"
 }
+
+*/
 
 tags = {
     name = "terraserver1"
