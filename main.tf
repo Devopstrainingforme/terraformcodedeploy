486708b5af2df99dc3099dbc90355e5816fb8e97
praveenjrm <praveen26cs@gmail.com>
2024-11-14 22:21:20 +0530

    updated the root module main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,15 @@ provider "aws" {
 module "ec2" {
   source = "./modules/ec2"
 }
+
+module "security_group" {
+  source = "./modules/security_group"
+}
+
+module "subnet" {
+  source = "./modules/subnet"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
