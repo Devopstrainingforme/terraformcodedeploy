02aff2f72f55a03e5918c74c9100e03a925cd188
praveenjrm <praveen26cs@gmail.com>
2024-11-14 21:12:17 +0530

    v3

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,11 +1,11 @@
 variable "imageid" {
     description = "default image id"
     type = string
-    default = ""
+    default = "ami-0866a3c8686eaeeba"
 }
 
 variable "terrainstype" {
     description = "Instance type used"
     type = string
-    default = ""
+    default = "t2.micro"
 }
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-imageid = "ami-0866a3c8686eaeeba"
-instance_type = "t2.micro"
