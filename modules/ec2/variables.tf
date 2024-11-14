04ad14d25351ee828e085bfc33575c709d2fa0f8
praveenjrm <praveen26cs@gmail.com>
2024-11-14 20:03:40 +0530

    code fix

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,11 +1,11 @@
 variable "imageid" {
     description = "default image id"
     type = string
-    default = "ami-0866a3c8686eaeeba"
+    default = "ami-04e914639d0cca79a"
 }
 
 variable "terrainstype" {
     description = "Instance type used"
     type = string
-    default = "t3.micro"
+    default = "t2.micro"
 }
